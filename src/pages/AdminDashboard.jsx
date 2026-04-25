085e5296fd6870c674b75a0513e83da2dc286be0
HYKZOUNE ELYSEE <hykzouneelisereunba@gmail.com>
2026-04-25 13:51:53 +0200

    fix cartes-bloquees route final

--- src/pages/AdminDashboard.jsx
+++ src/pages/AdminDashboard.jsx
@@ -130,7 +130,7 @@ const handleDecision = async (id, decision) => {
   switch (type) {
     case "reclamations": url = "/api/admin/chargeback/all"; break;
     case "cartebloques":
-url = "/api/admin/cartesbloquees";
+url = "/api/admin/cartes-bloquees";
 break;
     case "CarteAvale": url = "/api/admin/carteavale"; break;
     case "clients": url = "/api/admin/clients"; break;
