446aeec3dddabdc838195ca113702a100c23df49
HYKZOUNE ELYSEE <hykzouneelisereunba@gmail.com>
2026-04-25 14:48:40 +0200

    fix routes naming consistency cartesbloquees

--- src/pages/AdminDashboard.jsx
+++ src/pages/AdminDashboard.jsx
@@ -129,9 +129,7 @@ const handleDecision = async (id, decision) => {
   let url = "";
   switch (type) {
     case "reclamations": url = "/api/admin/chargeback/all"; break;
-    case "cartebloquees":
-url = "/api/admin/cartesbloquees";
-break;
+    case "cartebloquees": url = "/api/admin/cartesbloquees"; break;
     case "CarteAvale": url = "/api/admin/carteavale"; break;
     case "clients": url = "/api/admin/clients"; break;
     case "agents": url = "/api/admin/agents"; break;
